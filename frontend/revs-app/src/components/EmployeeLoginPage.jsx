c4b8afcaf615dc8ffa81a3af1eea06148e005627
Caleb Williamson <caleb7981@tamu.edu>
2022-11-03 00:14:00 -0500

    Created Header for the Home Page of our app.  Initialized components for the Employee Login Page and the Customer Checkout Page.

---
+++ frontend/revs-app/src/components/EmployeeLoginPage.jsx
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import CustomerHeader from "./CustomerHeader";
+
+const EmployeeLoginPage = () => {
+    return (
+        <div>
+            <CustomerHeader />
+            <div>Employee Login Page</div>  
+        </div>
+    )
+}
+
+export default EmployeeLoginPage;
---
+++ frontend/revs-app/src/routes/Checkout.js
@@ -0,0 +1,11 @@
+import CheckoutPage from "../components/CheckoutPage";
+
+const Checkout = () => {
+    return (
+        <div>
+            <CheckoutPage />
+        </div>
+    )
+}
+
+export default Checkout;
