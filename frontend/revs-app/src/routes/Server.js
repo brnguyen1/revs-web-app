1582d7c059ae0e49ea4e5207f90bef32de1461f2
bnguyen <bnguyen@tamu.edu>
2022-11-03 05:46:56 -0500

    employee tracker

---
+++ frontend/revs-app/src/routes/Server.js
@@ -0,0 +1,19 @@
+import EntityTable from "../components/EntityTable"
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import CustomerHeader from "../components/CustomerHeader";
+
+function Server() {
+    return (
+        <div>
+            <CustomerHeader />
+            <div className="container w-100">
+                <div className="table-responsive text-nowrap w-100">
+                    <EntityTable entityName="server" />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Server
