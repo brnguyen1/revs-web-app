ce6082ffffe9cbb3031da132ba689063ce8ecf68
Ivanmartinez22 <ivanrubalcava3@gmail.com>
2022-11-06 17:16:18 -0600

    updated gui files

--- frontend/revs-app/src/components/CheckoutPage.jsx
+++ frontend/revs-app/src/components/CheckoutPage.jsx
@@ -7,25 +7,74 @@ import { useState } from 'react';
 // import {Card} from '@mui/material';
 
 
-let menu_items=[ // dummy data will be substituted for data from data base
-    {
-        id: 1,
-        name: "Object0",
-        price: 10
-    },
-    {
-        id: 2,
-        name: "Object1",
-        price: 11
-    },
-    {
-        id: 3,
-        name: "Object2",
-        price: 12
-    },
+// let menu_items=[ // dummy data will be substituted for data from data base
+//     {
+//         id: 1,
+//         name: "Object0",
+//         price: 10
+//     },
+//     {
+//         id: 2,
+//         name: "Object1",
+//         price: 11
+//     },
+//     {
+//         id: 3,
+//         name: "Object2",
+//         price: 12
+//     },
 
-];
+// ];
 
+import axios from 'axios'
+let array = []
+async function fetch_data() {
+    var endpoint = 'http://localhost:4173/' + 'menu'
+    console.log(endpoint)
+    const res = await axios.get(endpoint)
+    return res
+}
+
+function parse_data() {
+    fetch_data().then(res => {
+        //console.log(res)
+        let tmp_headers = []
+        for (const [idx, field] of Object.entries(res.data.fields)) {
+            tmp_headers.push(field.name)
+        }
+        //console.log(tmp_headers)
+        // setHeaders(tmp_headers)
+        // setTData(res.data.rows)
+        //console.log(res.data.rows)
+       
+        
+        // data.push(parse_data());
+        // let tmp_rows = []
+        for(const [idx, field] of Object.entries(res.data.rows)){
+            //var row =  res.data.rows[idx];
+            // let object_array = Object.values(row);
+            // let iter = new MenuItem(array[0], array[1], array[2], array[3], array[4]);
+
+            // array.push({id: object_array[0], name: object_array[1], price: object_array[3]});
+            array.push({id: field.id, name: field.description, price: field.price})
+        
+        }
+        //console.log(tmp_rows)
+        // return tmp_rows;
+        // for(var i = 0; i < tmp_rows.length(); i++){
+            
+        // }
+
+    })
+
+}
+
+console.log(array);
+parse_data();
+
+console.log(array);
+
+let menu_items = array;
 
 
 
