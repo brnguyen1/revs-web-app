9c933aa51fa3dedc0c2227b529ac0630f97658be
Ivanmartinez22 <ivanrubalcava3@gmail.com>
2022-11-06 12:53:26 -0600

    missing file fix

--- frontend/revs-app/src/index.js
+++ frontend/revs-app/src/index.js
@@ -6,8 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.js';
-import $ from 'jquery';
-import Popper from 'popper.js';
+// import $ from 'jquery';
+// import Popper from 'popper.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
