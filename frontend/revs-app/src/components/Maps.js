5ffeb225942cef24618441a554b9818879388226
binhdocao <98185593+binhdocao@users.noreply.github.com>
2022-11-08 14:07:33 -0600

    map on screen, no api

--- frontend/revs-app/src/components/Maps.js
+++ frontend/revs-app/src/components/Maps.js
@@ -1,14 +1,53 @@
 import React from 'react'
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '500px',
+  height: '500px'
+};
+
+const center = {
+  lat: 30.612552,
+  lng: -96.341326
+};
 
 function Maps() {
-    let map_src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDfNjTUiHor5frrxETqjmmRFPlQFbxAkro&callback=initMap'
-    return (
-        <div className="maps">
-        <script async src={map_src}></script>
-        <h1> Im the map</h1>
-        
-        </div>
-    );
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: ""
+  })
+
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return isLoaded ? (
+    <center>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onLoad={onLoad}
+        mapTypeId='satellite'
+        onUnmount={onUnmount}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+      </center>
+  ) : <></>
 }
 
-export default Maps;
+export default React.memo(Maps)
