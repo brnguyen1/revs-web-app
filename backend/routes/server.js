663e3ce5435ce79f81442688e22eb79f3b36deea
bnguyen <bnguyen@tamu.edu>
2022-11-12 18:19:10 -0600

    created routes for each table on backend

---
+++ backend/routes/server.js
@@ -0,0 +1,11 @@
+var router = express.Router();
+var queries = require('../services/queries')
+
+router.get('/', function (req, res) {
+    var entity = "server"
+    queries.select_all_query(entity).then(data => {
+        res.json(data);
+    });
+});
+
+module.exports = router;
